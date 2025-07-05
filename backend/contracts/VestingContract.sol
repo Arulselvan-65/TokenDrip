7866a5702fffe278c2e6fe20bd83a07e07f2c5d2
Arulselvan M <arulselvan2244@gmail.com>
2025-07-05 12:43:02 +0530

    Added testcases for vesting contract

--- backend/contracts/VestingContract.sol
+++ backend/contracts/VestingContract.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.27;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
 
 contract VestingContract is Ownable {
 
@@ -18,6 +19,7 @@ contract VestingContract is Ownable {
         uint8 totalDays;
         uint totalTokens;
         uint startTime;
+        uint endTime;
         uint claimed;
         bool isActive;
     }
@@ -27,16 +29,24 @@ contract VestingContract is Ownable {
     event ScheduleCreated(address indexed to, uint time);
     event TokenClaimed(address indexed addr, uint amount);
 
+    error InvalidAddress(address _addr);
+    error ScheduleAlreadyExists(address _addr);
+    error ExceedsTotalSupply(uint256 requested, uint256 total);
+    error NoActiveSchedule();
+    error NoTokensToClaim();
+    error TokenTransferFailed();
+    error ScheduleCompleted();
 
     function createSchedule(address addr, uint8 _totalDays, uint _totalTokens) external onlyOwner {
-        require(addr != address(0), "Invalid Address");
-        require(!vestingSchedules[addr].isActive, "Schedule Exists");
-        require(token.balanceOf(address(this)) >= _totalTokens, "Insufficient balance");
+        require(addr != address(0), InvalidAddress(msg.sender));
+        require(!vestingSchedules[addr].isActive, ScheduleAlreadyExists(addr));
+        require(token.balanceOf(address(this)) >= _totalTokens, ExceedsTotalSupply(_totalTokens, token.balanceOf(address(this))));
 
         vestingSchedules[addr] = VestingSchedule({
             totalDays: _totalDays,
             totalTokens: _totalTokens,
             startTime: block.timestamp,
+            endTime: block.timestamp + (_totalDays * 1 days),
             claimed: 0,
             isActive: true
         });
@@ -46,22 +56,23 @@ contract VestingContract is Ownable {
     }
 
     function claimTokens() external {
-        require(vestingSchedules[msg.sender].isActive, "No active schedule");
+        require(vestingSchedules[msg.sender].isActive, NoActiveSchedule());
+        require(vestingSchedules[msg.sender].endTime >= block.timestamp, ScheduleCompleted());
         VestingSchedule storage vestingSchedule = vestingSchedules[msg.sender];
         uint claimable = calculateVestedAmount(msg.sender) - vestingSchedule.claimed;
-        require(claimable > 0, "No tokens to claim");
+        require(claimable > 0, NoTokensToClaim());
         vestingSchedule.claimed += claimable;
         if(vestingSchedule.claimed == vestingSchedule.totalTokens){
             activeSchedules--;
             vestingSchedule.isActive = false;
         }
-        require(token.transfer(msg.sender, claimable), "Token transfer failed");
+        require(token.transfer(msg.sender, claimable), TokenTransferFailed());
 
         emit TokenClaimed(msg.sender, claimable);
     }
 
     function calculateVestedAmount(address _addr) internal view returns(uint){
-        require(vestingSchedules[_addr].isActive, "No active schedule");
+        require(vestingSchedules[_addr].isActive, NoActiveSchedule());
         VestingSchedule memory vestingSchedule = vestingSchedules[_addr];
         uint elapsedDays = (block.timestamp - vestingSchedule.startTime) / 1 days;
         if(elapsedDays >= vestingSchedule.totalDays){
@@ -73,7 +84,7 @@ contract VestingContract is Ownable {
     }
 
     function claimRemainTokens() external onlyOwner {
-        require(token.balanceOf(address(this)) > 0, "No tokens to claim");
-        require(token.transfer(msg.sender, token.balanceOf(address(this))), "Token transfer failed");
+        require(token.balanceOf(address(this)) > 0, NoTokensToClaim());
+        require(token.transfer(msg.sender, token.balanceOf(address(this))), TokenTransferFailed());
     }
 }
--- backend/test/ContractTest.js
+++ backend/test/ContractTest.js
@@ -1,15 +1,11 @@
-const {loadFixture, time } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-const { ethers, ignition } = require("hardhat");
+const { ethers } = require("hardhat");
 const { expect } = require("chai");
-const {extendConfig} = require("hardhat/config");
-
 
 describe("TokenDrip", function () {
   let tokenContract;
   let vestingContract;
   let owner, addr1, addr2;
-  const DECIMALS = 10n**18n
+  const DECIMALS = 10n**18n;
 
   beforeEach(async function () {
     [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
@@ -50,5 +46,94 @@ describe("TokenDrip", function () {
         await vestingContract.createSchedule(addr1, 10, 10n * DECIMALS);
         expect(await vestingContract.vestingSchedules(addr1)).to.not.equal(null);
      });
+
+     it("Should set amount and days correctly", async function() {
+       await tokenContract.connect(owner).mint(vestingContract.target, 20n);
+       await vestingContract.createSchedule(addr1, 10, 10n * DECIMALS);
+       const result = await vestingContract.vestingSchedules(addr1);
+       expect(await vestingContract.vestingSchedules(addr1)).to.not.equal(null);
+       expect(result[0]).to.equal(10n);
+       expect(result[1]).to.equal(10n * DECIMALS);
+     });
+
+     it("Should revert when creating schedule with same address", async function() {
+       await tokenContract.connect(owner).mint(vestingContract.target, 20n);
+       await vestingContract.createSchedule(addr1, 10, 10n * DECIMALS);
+       await expect(vestingContract.createSchedule(addr1, 10, 20n * DECIMALS))
+           .to.be.revertedWithCustomError(vestingContract, "ScheduleAlreadyExists")
+           .withArgs(addr1);
+     });
+
+     it("Should revert when requested balance is higher then available balance", async function() {
+         await tokenContract.connect(owner).mint(vestingContract.target, 10n);
+         await vestingContract.createSchedule(addr1, 10, 10n * DECIMALS);
+         await expect(vestingContract.createSchedule(addr2, 10, 20n * DECIMALS))
+             .to.be.revertedWithCustomError(vestingContract, "ExceedsTotalSupply")
+             .withArgs(20n * DECIMALS, 10n * DECIMALS);
+     });
+
+     it("Should emit event when schedule created", async function() {
+         await tokenContract.connect(owner).mint(vestingContract.target, 10n);
+         const block = await ethers.provider.getBlock("latest");
+         await expect(vestingContract.createSchedule(addr1, 10, 10n * DECIMALS))
+             .to.emit(vestingContract, "ScheduleCreated")
+             .withArgs(addr1, BigInt(block.timestamp + 1));
+     });
+
+     it("Should claim tokens after creation",  async function() {
+         await tokenContract.connect(owner).mint(vestingContract.target, 10n);
+         await vestingContract.createSchedule(addr1, 10, 10n * DECIMALS);
+         await vestingContract.connect(addr1).claimTokens();
+         await expect(await tokenContract.balanceOf(addr1)).to.equal(1n * DECIMALS);
+     });
+
+     it("Should revert when trying to claim on a same day", async function() {
+         await tokenContract.connect(owner).mint(vestingContract.target, 10n);
+         await vestingContract.createSchedule(addr1, 10, 10n * DECIMALS);
+         await vestingContract.connect(addr1).claimTokens();
+         await expect(vestingContract.connect(addr1).claimTokens())
+             .to.be.revertedWithCustomError(vestingContract, "NoTokensToClaim");
+     });
+
+     it("Should claim tokens next day", async function(){
+         await tokenContract.connect(owner).mint(vestingContract.target, 10n);
+         await vestingContract.createSchedule(addr1, 10, 10n * DECIMALS);
+         await vestingContract.connect(addr1).claimTokens();
+         await ethers.provider.send('evm_increaseTime', [1 * 24 * 60 * 60]);
+         await ethers.provider.send('evm_mine');
+         await expect(vestingContract.connect(addr1).claimTokens())
+             .to.emit(vestingContract, "TokenClaimed").withArgs(addr1, 1n * DECIMALS);
+     });
+
+     it("Should claim the elapsed days tokens", async function() {
+         await tokenContract.connect(owner).mint(vestingContract.target, 10n);
+         await vestingContract.createSchedule(addr1, 10, 10n * DECIMALS);
+         await vestingContract.connect(addr1).claimTokens();
+         await ethers.provider.send('evm_increaseTime', [5 * 24 * 60 * 60]);
+         await ethers.provider.send('evm_mine');
+         await expect(vestingContract.connect(addr1).claimTokens())
+             .to.emit(vestingContract, "TokenClaimed").withArgs(addr1, 5n * DECIMALS);
+     });
+
+     it("Should revert transactions after all the tokens are claimed", async function(){
+         await tokenContract.connect(owner).mint(vestingContract.target, 10n);
+         await vestingContract.createSchedule(addr1, 10, 10n * DECIMALS);
+         await vestingContract.connect(addr1).claimTokens();
+         await ethers.provider.send('evm_increaseTime', [9 * 24 * 60 * 60]);
+         await ethers.provider.send('evm_mine');
+         await expect(vestingContract.connect(addr1).claimTokens());
+         await expect(vestingContract.connect(addr1).claimTokens())
+             .to.be.revertedWithCustomError(vestingContract, "NoActiveSchedule");
+     });
+
+      it("Should revert when trying to claim tokens after the scheduled days", async function(){
+          await tokenContract.connect(owner).mint(vestingContract.target, 10n);
+          await vestingContract.createSchedule(addr1, 5, 10n * DECIMALS);
+          await vestingContract.connect(addr1).claimTokens();
+          await ethers.provider.send('evm_increaseTime', [12 * 24 * 60 * 60]);
+          await ethers.provider.send('evm_mine');
+          await expect(vestingContract.connect(addr1).claimTokens())
+              .to.be.revertedWithCustomError(vestingContract, "ScheduleCompleted");
+      });
   });
 });
